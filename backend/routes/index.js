ec3ad394a6c303bcdb1cbe571a09be7f2832f7fd
TahaRizwan <tahabhaiyag@gmail.com>
2022-08-27 20:46:27 +0500

    Backend completed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,64 +1,26 @@
 import express from 'express'
+import {
+  createWorkout,
+  deleteWorkout,
+  getAllWorkouts,
+  getSingleWorkout,
+  updateWorkout,
+} from '../controllers/workoutController.js'
 import { Workout } from '../models/workouts.js'
 
 export const router = express.Router()
 
 // GET ALL WORKOUT
-router.get('/', async (req, res) => {
-  try {
-    const workout = await Workout.find()
-    res.json(workout).status(200)
-  } catch (error) {
-    console.log(`error ${error}`)
-    res.json(error).status(404)
-  }
-})
+router.get('/', getAllWorkouts)
 
 // GET SINGLE WORKOUT
-router.get('/:id', async (req, res) => {
-  try {
-    const workout = await Workout.findOne({ _id: req.params.id })
-    res.json(workout).status(200)
-  } catch (error) {
-    console.log(`error ${error}`)
-    res.json(error).status(404)
-  }
-})
+router.get('/:id', getSingleWorkout)
 
 // POST A NEW WORKOUT
-router.post('/', async (req, res) => {
-  const { title, load, reps } = req.body
-  try {
-    const workout = await Workout.create({ title, load, reps })
-    res.json(workout).status(200)
-  } catch (error) {
-    console.log(`error ${error}`)
-    res.json(error).status(404)
-  }
-})
+router.post('/', createWorkout)
 
 // DELETE A WORKOUT
-router.delete('/:id', async (req, res) => {
-  try {
-    const workout = await Workout.deleteOne({ _id: req.params.id })
-    res.json({ msg: 'Deleted Successfully' }).status(200)
-  } catch (error) {
-    console.log(`error ${error}`)
-    res.json(error).status(404)
-  }
-})
+router.delete('/:id', deleteWorkout)
 
 // PATCH A WORKOUT
-router.patch('/:id', async (req, res) => {
-  const { title, load, reps } = req.body
-  try {
-    const workout = await Workout.findOneAndUpdate(
-      { _id: req.params.id },
-      { title, load, reps }
-    )
-    res.json({ msg: 'Updated Successfully' }).status(200)
-  } catch (error) {
-    console.log(`error ${error}`)
-    res.json(error).status(404)
-  }
-})
+router.patch('/:id',updateWorkout)
