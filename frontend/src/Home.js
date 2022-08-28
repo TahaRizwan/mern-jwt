6b949e54fc3cf0e03756fb086e1d1834bd89478b
taharizwan <tahabhaiyag@gmail.com>
2022-08-28 19:45:12 +0500

    App Added

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,24 +1,39 @@
 import React, { useEffect, useState } from 'react'
+import WorkoutDetails from './components/WorkoutDetails'
+import WorkoutForm from './components/WorkoutForm'
+import { useDispatch, useSelector } from 'react-redux'
+import { getWorkout } from './workout/workoutAction'
+import CircularProgress from '@mui/material/CircularProgress'
+import { Box } from '@mui/material'
+
 
 const Home = () => {
   const [workouts, setWorkouts] = useState([])
+  const dispatch = useDispatch()
+  const data = useSelector((state) => state.workout.workout)
+  const loading = useSelector((state) => state.workout.loading)
   useEffect(() => {
     const fetchAllWorkout = async () => {
-      const response = await fetch(
-        `https://mern-jwt-app.herokuapp.com/api/workouts`
-      )
-      const data = await response.json()
-      setWorkouts(data)
-      console.log(data) 
+      dispatch(getWorkout)
     }
     fetchAllWorkout()
-  }, [])
+  }, [dispatch])
+
+  useEffect(() => setWorkouts(data), [data])
   return (
     <div className='home'>
       <div className='workouts'>
+        {loading && (
+          <Box sx={{ display: 'flex' }}>
+            <CircularProgress />
+          </Box>
+        )}
         {workouts &&
-          workouts.map((workout) => <p key={workout._id}>{workout.title}</p>)}
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
       </div>
+      <WorkoutForm />
     </div>
   )
 }
